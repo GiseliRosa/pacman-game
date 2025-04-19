42d45878eeb7e554526d39bd93e084a31eff087c
Daniel H <daniel.neves0021@gmail.com>
2025-04-19 16:40:29 -0300

    feat: vidas imagem da cereja e alteracao no mapa

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Diagnostics.Metrics;
-using System.Runtime.InteropServices.JavaScript;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-
-using System.Windows.Threading; // import the threading name space to use the dispatcher time
 
 namespace PAC_Man_Game_WPF_MOO_ICT
 {
@@ -22,33 +12,28 @@ namespace PAC_Man_Game_WPF_MOO_ICT
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-        /*gameTimer: Controla o loop do jogo, atualizando os movimentos e verificações de colisão.
-          goLeft, goRight, goDown, goUp: Determinam para onde o Pac-Man está se movendo.
-          noLeft, noRight, noDown, noUp: Impedem que o Pac-Man atravesse paredes.*/
+    /*gameTimer: Controla o loop do jogo, atualizando os movimentos e verificações de colisão.
+      goLeft, goRight, goDown, goUp: Determinam para onde o Pac-Man está se movendo.
+      noLeft, noRight, noDown, noUp: Impedem que o Pac-Man atravesse paredes.*/
     {
         DispatcherTimer gameTimer = new DispatcherTimer(); // create a new instance of the dispatcher timer called game timer
-
         bool goLeft, goRight, goDown, goUp; // 4 boolean created to move player in 4 direction
         bool noLeft, noRight, noDown, noUp; // 4 more boolean created to stop player moving in that direction
-
-        int speed = 8; // Velocidade do Pac-Man.
-
+        int speed = 7; // Velocidade do Pac-Man.
+        short life = 3; // Vida do fia da mãe
         Rect pacmanHitBox; //Área de colisão do Pac-Man.
-
-        int ghostSpeed = 10; // Velocidade dos fantasmas.
+        int ghostSpeed = 4; // Velocidade dos fantasmas.
         int ghostMoveStep = 160; // Distância máxima que os fantasmas podem se mover antes de mudar de direção.
         int currentGhostStep; // current movement limit for the ghosts
         int score = 0; // score keeping integer
-
-
+        private Random rand = new Random();
+        private Dictionary<Rectangle, string> ghostDirections = new Dictionary<Rectangle, string>();
 
         public MainWindow()
         {
             InitializeComponent();
-
             GameSetUp(); // run the game set up function
         }
-
         /*Este evento captura o pressionamento de teclas e define a direção do Pac-Man.
         Se Left for pressionado e noLeft for false, o Pac-Man começa a se mover para a esquerda.
         goRight, goUp, goDown são desativados para que ele não se mova em múltiplas direções ao mesmo tempo.
@@ -57,15 +42,12 @@ namespace PAC_Man_Game_WPF_MOO_ICT
         private void CanvasKeyDown(object sender, KeyEventArgs e)
         {
             // this is the key down event
-
             if (e.Key == Key.Left && noLeft == false)
             {
                 // if the left key is down and the boolean noLeft is set to false
                 goRight = goUp = goDown = false; // set rest of the direction booleans to false
                 noRight = noUp = noDown = false; // set rest of the restriction boolean to false
-
                 goLeft = true; // set go left true
-
                 pacman.RenderTransform = new RotateTransform(-180, pacman.Width / 2, pacman.Height / 2); // rotate the pac man image to face left
             }
 
@@ -74,21 +56,16 @@ namespace PAC_Man_Game_WPF_MOO_ICT
                 // if the right key pressed and no right boolean is false
                 noLeft = noUp = noDown = false; // set rest of the direction boolean to false
                 goLeft = goUp = goDown = false; // set rest of the restriction boolean to false
-
                 goRight = true; // set go right to true
-
                 pacman.RenderTransform = new RotateTransform(0, pacman.Width / 2, pacman.Height / 2); // rotate the pac man image to face right
-
             }
 
             if (e.Key == Key.Up && noUp == false)
             {
                 // if the up key is pressed and no up is set to false
                 noRight = noDown = noLeft = false; // set rest of the direction boolean to false
-                goRight = goDown = goLeft = false; // set rest of the restriction boolean to false
-
+                goRight = goDown = goLeft = false; // set rest of the restriction boolean to 
                 goUp = true; // set go up to true
-
                 pacman.RenderTransform = new RotateTransform(-90, pacman.Width / 2, pacman.Height / 2); // rotate the pac man character to face up
             }
 
@@ -97,25 +74,17 @@ namespace PAC_Man_Game_WPF_MOO_ICT
                 // if the down key is press and the no down boolean is false
                 noUp = noLeft = noRight = false; // set rest of the direction boolean to false
                 goUp = goLeft = goRight = false; // set rest of the restriction boolean to false
-
                 goDown = true; // set go down to true
-
                 pacman.RenderTransform = new RotateTransform(90, pacman.Width / 2, pacman.Height / 2); // rotate the pac man character to face down
             }
-
-
         }
-
         private void GameSetUp()
         {
             // this function will run when the program loads
-
             MyCanvas.Focus(); // set my canvas as the main focus for the program
-
-            gameTimer.Tick += GameLoop; // link the game loop event to the time tick
-            gameTimer.Interval = TimeSpan.FromMilliseconds(20); // set time to tick every 20 milliseconds
+            gameTimer.Tick += GameLoop!; // link the game loop event to the time tick
+            gameTimer.Interval = TimeSpan.FromMilliseconds(30); // set time to tick every 20 milliseconds
             gameTimer.Start(); // start the time
-
             currentGhostStep = ghostMoveStep; // Configura o movimento inicial dos fantasmas
 
             // below pac man and the ghosts images are being imported from the images folder and then we are assigning the image brush to the rectangles
@@ -135,16 +104,24 @@ namespace PAC_Man_Game_WPF_MOO_ICT
             pinkGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pink.jpg"));
             pinkGuy.Fill = pinkGhost;
 
+            // Isso aqui são as texturas das vidas;
+            ImageBrush lifeOneImage = new ImageBrush();
+            lifeOneImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pacman.jpg"));
+            lifeOne.Fill = lifeOneImage;
+
+            ImageBrush lifeTwoImage = new ImageBrush();
+            lifeTwoImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pacman.jpg"));
+            lifeTwo.Fill = lifeTwoImage;
+
+            ImageBrush lifeThreeImage = new ImageBrush();
+            lifeThreeImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pacman.jpg"));
+            lifeThree.Fill = lifeThreeImage;
 
         }
-
         private void GameLoop(object sender, EventArgs e)
         {
-
             // this is the game loop event, this event will control all of the movements, outcome, collision and score for the game
-
             txtScore.Content = "Score: " + score; // show the scoreo to the txtscore label. 
-
             // start moving the character in the movement directions
 
             if (goRight)
@@ -169,33 +146,35 @@ namespace PAC_Man_Game_WPF_MOO_ICT
             }
             // end the movement 
 
-
             //Evita que Pac-Man saia da tela.
             if (goDown && Canvas.GetTop(pacman) + 80 > Application.Current.MainWindow.Height)
             {
                 // if pac man is moving down the position of pac man is grater than the main window height then stop down movement
-                noDown = true;
-                goDown = false;
+                //noDown = true;
+                //goDown = false;
+                Canvas.SetTop(pacman, 0);
             }
             if (goUp && Canvas.GetTop(pacman) < 1)
             {
                 // is pac man is moving and position of pac man is less than 1 then stop up movement
-                noUp = true;
-                goUp = false;
+                //noUp = true;
+                //goUp = false;
+                Canvas.SetTop(pacman, Application.Current.MainWindow.Height);
             }
             if (goLeft && Canvas.GetLeft(pacman) - 10 < 1)
             {
                 // if pac man is moving left and pac man position is less than 1 then stop moving left
-                noLeft = true;
-                goLeft = false;
+                //noLeft = true;
+                //goLeft = false;
+                Canvas.SetLeft(pacman, Application.Current.MainWindow.Height);
             }
             if (goRight && Canvas.GetLeft(pacman) + 70 > Application.Current.MainWindow.Width)
             {
                 // if pac man is moving right and pac man position is greater than the main window then stop moving right
-                noRight = true;
-                goRight = false;
+                //noRight = true;
+                //goRight = false;
+                Canvas.SetLeft(pacman, 0);
             }
-
             pacmanHitBox = new Rect(Canvas.GetLeft(pacman), Canvas.GetTop(pacman), pacman.Width, pacman.Height); // asssign the pac man hit box to the pac man rectangle
 
             // below is the main game loop that will scan through all of the rectangles available inside of the game
@@ -261,8 +240,8 @@ namespace PAC_Man_Game_WPF_MOO_ICT
                     // check if pac man collides with the ghost 
                     if (pacmanHitBox.IntersectsWith(hitBox))
                     {
-                        // if collision has happened, then end the game by calling the game over function and passing in the message
-                        GameOver("Ghosts got you, click ok to play again");
+                        // Lógica para renascer o pacman quando tomar dano
+                        DeathLogic();
                     }
 
                     // if there is a rectangle called orange guy in the game
@@ -270,7 +249,6 @@ namespace PAC_Man_Game_WPF_MOO_ICT
                     {
                         // move that rectangle to towards the left of the screen
                         Canvas.SetLeft(x, Canvas.GetLeft(x) - ghostSpeed);
-
                     }
                     else
                     {
@@ -288,14 +266,12 @@ namespace PAC_Man_Game_WPF_MOO_ICT
                         currentGhostStep = ghostMoveStep;
                         // reverse the ghost speed integer
                         ghostSpeed = -ghostSpeed;
-
                     }
                 }
             }
 
 
             // if the player collected 85 coins in the game
-
             if (score == 85)
             {
                 // show game over function with the you win message
@@ -304,9 +280,47 @@ namespace PAC_Man_Game_WPF_MOO_ICT
 
 
         }
-        private Random rand = new Random();
-        private Dictionary<Rectangle, string> ghostDirections = new Dictionary<Rectangle, string>();
 
+        private void DeathLogic() 
+        {
+            if (life == 3)
+            {
+                lifeOne.Visibility = Visibility.Hidden;
+                life--;
+                Canvas.SetLeft(pacman, 417);
+                Canvas.SetTop(pacman, 452);
+                goRight = false;
+                goDown = false;
+                goUp = false;
+                goLeft = false;
+            }
+            else if (life == 2)
+            {
+                lifeTwo.Visibility = Visibility.Hidden;
+                life--;
+                Canvas.SetLeft(pacman, 417);
+                Canvas.SetTop(pacman, 452);
+                goRight = false;
+                goDown = false;
+                goUp = false;
+                goLeft = false;
+            }
+            else if (life == 1)
+            {
+                lifeThree.Visibility = Visibility.Hidden;
+                life--;
+                Canvas.SetLeft(pacman, 417);
+                Canvas.SetTop(pacman, 452);
+                goRight = false;
+                goDown = false;
+                goUp = false;
+                goLeft = false;
+            }
+            else
+            {
+                GameOver("O Fantasma te tocou, recomece! Aff!");
+            }
+        }
         private void MoveRandomGhost(Rectangle ghost)
         {
             double ghostX = Canvas.GetLeft(ghost);
@@ -339,7 +353,6 @@ namespace PAC_Man_Game_WPF_MOO_ICT
 
             // Criamos a hitbox para verificar colisão antes de mover
             Rect newGhostHitBox = new Rect(newGhostX, newGhostY, ghost.Width, ghost.Height);
-
             foreach (var wall in MyCanvas.Children.OfType<Rectangle>())
             {
                 if ((string)wall.Tag == "wall")
@@ -359,29 +372,25 @@ namespace PAC_Man_Game_WPF_MOO_ICT
             Canvas.SetLeft(ghost, newGhostX);
             Canvas.SetTop(ghost, newGhostY);
         }
-
         // Método auxiliar para obter uma direção aleatória
         private string GetRandomDirection()
         {
             string[] directions = { "up", "down", "left", "right" };
             return directions[rand.Next(directions.Length)];
         }
-
-
-
         private void GameOver(string message)
         {
             // inside the game over function we passing in a string to show the final message to the game
             gameTimer.Stop(); // stop the game timer
-            MessageBox.Show(message, "The Pac Man Game WPF MOO ICT"); // show a mesage box with the message that is passed in this function
+            MessageBox.Show(message, "Pacman da Giseli (Adaptado)"); // show a mesage box with the message that is passed in this function
 
             // when the player clicks ok on the message box
             // restart the application
-            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            //System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            //Application.Current.Shutdown();
+            System.Diagnostics.Process.Start(System.Diagnostics.Process.GetCurrentProcess().MainModule!.FileName);
             Application.Current.Shutdown();
         }
-
-
 
     }
 }
